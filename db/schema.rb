3025a6acdd4302c5d8c0231c651ede6163dc0f24
Iren Liber <irr.morgo@gmail.com>
2016-05-29 13:20:43 +0600

    created Note model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160529051247) do
+ActiveRecord::Schema.define(version: 20160529071836) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
