d2063aa79830b951a60b03677bf08555b800ac5b
irenliber <irr.morgo@gmail.com>
2016-05-31 11:20:09 +0600

    changed notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,53 @@
 class NotesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_note, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @notes = Note.where(user_id: current_user)
+  end
+
+  def new
+    @note = current_user.notes.build
+  end
+
+  def create
+    @note = current_user.notes.build(note_params)
+
+    if @note.save
+      redirect_to @note
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+
+  end
+
+  def update
+    if note.update(note_params)
+      redirect_to notes_path
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @note.destroy
+    redirect_to notes_path
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :content)
+  end
+
+  def find_note
+    @note = Note.find(params[:id])
+  end
 end
