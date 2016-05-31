e5a6e99414a043eabc97385c65008b3eae169629
irenliber <irr.morgo@gmail.com>
2016-05-31 11:19:33 +0600

    added field 'user_id' to note moddel

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160529071836) do
+ActiveRecord::Schema.define(version: 20160530140710) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "roles", force: :cascade do |t|
