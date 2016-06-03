c599b696388b8f1da737cf034d11f0c15dc58454
irenliber <irr.morgo@gmail.com>
2016-06-03 10:17:10 +0600

    added load_and_authorize_resource to notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,7 @@
 class NotesController < ApplicationController
   before_action :authenticate_user!
   before_action :find_note, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   def index
     if current_user.has_role? :admin
